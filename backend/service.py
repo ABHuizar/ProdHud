9a4a3405c2035784984ec7b1363c6132e13b0d3d
ABHuizar <andreebernal.h@gmail.com>
2025-07-08 19:22:45 -0700

    Fix: usar host y puerto compatibles con Render

--- backend/service.py
+++ backend/service.py
@@ -164,5 +164,7 @@ def dashboard():
 
     return jsonify(result)
 
+#PRUEBA COMMIT
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
